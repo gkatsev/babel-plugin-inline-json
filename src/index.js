af525661eea95523734b8271e594644e72457f36
Matt Williams <mwilliams@change.org>
2016-04-25 10:23:44 -0700

    Add matchPattern option for config filename

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ export default function({ Plugin, types: t }) {
   return new Plugin('inline-json-config-values', {
     visitor: {
       MemberExpression: function MemberExpression(node) {
+        var re = new RegExp(file.opts.extra['inline-json-config-values'].matchPattern, 'g');
         if (t.isCallExpression(node.object) &&
             t.isIdentifier(node.object.callee, { name: 'require' }) &&
             t.isLiteral(node.object.arguments[0]) &&
-            node.object.arguments[0].value.match(/server-config/)) {
+            node.object.arguments[0].value.match(re)) {
           let srcPath = path.resolve(this.state.opts.filename);
           let pkg = require(path.join(srcPath, '..', node.object.arguments[0].value));
           let value = pkg[node.property.name];
