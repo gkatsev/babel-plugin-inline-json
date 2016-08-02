a4986b19cf77e2645ec78bf21f6717d4d8a44344
Gary Katsevman <git@gkatsev.com>
2016-08-02 15:30:57 -0400

    update tests

--- test/fixtures/example/actual.js
+++ test/fixtures/example/actual.js
@@ -1,3 +1,5 @@
 var foo = require('config').foo;
+var baz = require('babel-cli/package.json').version;
 
 console.log(foo);
+console.log(baz);
--- test/fixtures/example/expected.js
+++ test/fixtures/example/expected.js
@@ -1,3 +1,5 @@
 var foo = 'bar';
+var baz = '6.11.4';
 
 console.log(foo);
+console.log(baz);
