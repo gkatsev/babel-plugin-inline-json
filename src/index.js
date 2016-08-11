17bb7b30dc204c6fe87a990a88c1966ef0a2a15e
Gary Katsevman <git@gkatsev.com>
2016-08-11 11:17:44 -0400

    Json from deps (#1)
    * default to '.json$' match pattern
    
    * add missing dev deps
    
    * add watch task for babel watch
    
    * add ability to require json from dependencies
    
    * update tests

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import nodePath from 'path';
+import fs from 'fs';
+import resolve from 'resolve';
 
 export default function ({types: t}) {
   return {
     visitor: {
       MemberExpression(path, state) {
-        var {node, file} = path;
-        var re = new RegExp(state.opts.matchPattern, 'g');
+        let {node, file} = path;
+        let re = new RegExp(state.opts.matchPattern, 'g');
 
         if (t.isCallExpression(node.object) &&
             t.isIdentifier(node.object.callee, { name: 'require' }) &&
             t.isLiteral(node.object.arguments[0]) &&
             node.object.arguments[0].value.match(re)) {
           let srcPath = nodePath.resolve(state.file.opts.filename);
-          let pkg = require(nodePath.join(srcPath, '..', node.object.arguments[0].value));
-          let value = pkg[node.property.name];
+          let requireText = node.object.arguments[0].value;
 
+
+          let jsonPath = nodePath.join(srcPath, '..', requireText);
+          let pkg;
+          if (fs.existsSync(jsonPath + '.json')) {
+            pkg = require(jsonPath);
+          } else {
+            pkg = require(resolve.sync(requireText, {
+              basedir: nodePath.dirname(srcPath)
+            }));
+          }
+
+          let value = pkg[node.property.name];
           path.replaceWith(t.expressionStatement(t.valueToNode(value)));
         }
       }
